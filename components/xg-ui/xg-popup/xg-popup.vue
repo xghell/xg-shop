de71adfa5fcc4115a3386e4db5bc9a832096c38a
xghell <605301193@qq.com>
2020-04-14 18:35:39 +0800

    新增xg-pop组件

--- components/xg-ui/xg-popup/xg-popup.vue
+++ components/xg-ui/xg-popup/xg-popup.vue
@@ -1,184 +1,214 @@
 <template>
-	<view v-if="showPopup" class="uni-popup" @touchmove.stop.prevent="clear">
-		<uni-transition :mode-class="['fade']" :styles="maskClass" :duration="duration" :show="showTrans" @click="onTap" />
-		<uni-transition :mode-class="ani" :styles="transClassInner" :duration="duration" :show="showTrans">
-				<slot />
-		</uni-transition>
+	<view v-if="showPopup" class="popup" @tap="maskTap">
+		<view class="mask" :class="maskAnimationClass"></view>
+		<view :class="[contentAnimation, contentAnimationCommonClass, contentAnimationClass]">
+			<slot></slot>
+		</view>
 	</view>
 </template>
 
 <script>
-	/**
-	 * PopUp 弹出层
-	 * @description 弹出层组件，为了解决遮罩弹层的问题
-	 * @tutorial https://ext.dcloud.net.cn/plugin?id=329
-	 * @property {String} type = [top|center|bottom] 弹出方式
-	 * 	@value top 顶部弹出
-	 * 	@value center 中间弹出
-	 * 	@value bottom 底部弹出
-	 * @property {Boolean} animation = [ture|false] 是否开启动画
-	 * @property {Boolean} maskClick = [ture|false] 蒙版点击是否关闭弹窗
-	 * @event {Function} change 打开关闭弹窗触发，e={show: false}
-	 */
 	export default {
 		name: 'XgPopup',
 		props: {
-			// 开启动画
-			animation: {
-				type: Boolean,
-				default: true
-			},
 			// 弹出层类型，可选值，top: 顶部弹出层；bottom：底部弹出层；center：全屏弹出层；
 			//right: 右侧弹出；left: 左侧弹出；
 			type: {
 				type: String,
 				default: 'center'
 			},
-			// maskClick
-			maskClick: {
-				type: Boolean,
-				default: true
-			}
 		},
 		data() {
 			return {
-				duration: undefined,
-				ani: [],
 				showPopup: false,
-				showTrans: false,
-				maskClass: {
-					'position': 'absolute',
-					'bottom': 0,
-					'top': 0,
-					'left': 0,
-					'right': 0,
-					'backgroundColor': 'rgba(0, 0, 0, 0.4)'
-				},
-				transClass: {},
-			}
-		},
-		computed: {
-			transClassInner() {
-				return {
-					display: 'flex',
-					flexDirection: 'row',
-					...this.transClass,
-				}
+				maskAnimationClass: '',
+				contentAnimation: '',
+				contentAnimationCommonClass: '',
+				contentAnimationClass: '',
 			}
 		},
 		created() {
-			if (this.animation) {
-				this.duration = 300
-			} else {
-				this.duration = 0
-			}
-			
-			switch (this.type) {
-				case 'top':
-					this.ani = ['slide-top']
-					this.transClass = {
-						'position': 'absolute',
-						'top': 0,
-						'left': 0,
-						'right': 0,
-					}
-					break
-				case 'bottom':
-					this.ani = ['slide-bottom']
-					this.transClass = {
-						'position': 'absolute',
-						'left': 0,
-						'right': 0,
-						'bottom': 0
-					}
-					break
-				case 'left':
-					this.ani = ['slide-left']
-					this.transClass = {
-						'position': 'absolute',
-						'left': 0,
-						'top': 0,
-						'bottom': 0,
-					}
-					break;
-				case 'right':
-					this.ani = ['slide-right']
-					this.transClass = {
-						'position': 'absolute',
-						'top': 0,
-						'right': 0,
-						'bottom': 0
-					}
-					break;
-				case 'center':
-					this.ani = ['zoom-out', 'fade']
-					this.transClass = {
-						'position': 'absolute',
-					}
-			
-					break
-			}
+			this.maskAnimationClass = 'mask';
+			this.contentAnimationCommonClass = this.type;
 		},
 		methods: {
-			clear(e) {
-				e.stopPropagation();
-			},
 			open() {
-				this.showPopup = true
-				this.$nextTick(() => {
-					clearTimeout(this.timer)
-					this.timer = setTimeout(() => {
-						this.showTrans = true
-					}, 50);
-				})
-				this.$emit('change', {
-					show: true
-				})
-			},
-			close(type) {
-				this.showTrans = false
-				this.$nextTick(() => {
-					clearTimeout(this.timer)
-					this.timer = setTimeout(() => {
-						this.$emit('change', {
-							show: false
-						})
-						this.showPopup = false
-					}, 400)
+				this.showPopup = true;
+				
+				this.$nextTick(function () {
+					this.contentAnimationClass = this.type + '-init';
+					this.contentAnimation = 'content';
+					setTimeout(() => {
+						this.maskAnimationClass = 'mask-in';
+						this.contentAnimationClass = this.type + '-in';
+					}, 200);
 				})
 			},
-			onTap() {
-				if (!this.maskClick) return
-				this.close()
+			close() {
+				this.maskAnimationClass = 'mask-out';
+				this.contentAnimationClass = this.type + '-out';
+				
+				this.$nextTick(function () {
+					setTimeout(() => {
+						this.showPopup = false;
+					}, 300);
+				})
+			},
+			
+			maskTap() {
+				this.close();
 			}
-		}
+		},
 	}
 </script>
+
 <style lang="scss" scoped>
-	.uni-popup {
+	.popup {
 		position: fixed;
-		/* #ifdef H5 */
-		top: var(--window-top);
-		/* #endif */
-		/* #ifndef H5 */
 		top: 0;
-		/* #endif */
+		right: 0;
 		bottom: 0;
 		left: 0;
-		right: 0;
-		/* #ifndef APP-NVUE */
-		z-index: 99;
-		/* #endif */
-		
-		border-width: 2rpx;
-		border-color: red;
 		
 		/* #ifndef APP-PLUS-NVUE */
 		display: flex;
+		z-index: 100000;
 		/* #endif */
 		flex-direction: row;
 		justify-content: center;
 		align-items: center;
 	}
-
+	
+	.mask {
+		transition-property: transform, opacity;
+		transition-duration: 0.3s;
+		transition-delay: 0s;
+		transition-timing-function: ease;
+		
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		opacity: 0;
+		background-color: #000000;
+	}
+	.mask-in {
+		opacity: 0.3;
+	}
+	.mask-out {
+		opacity: 0;
+	}
+	
+	.content {
+		transition-property: transform, opacity;
+		transition-duration: 0.3s;
+		transition-delay: 0s;
+		transition-timing-function: ease;
+	}
+	.center {
+		opacity: 0;
+	}
+	.center-init {
+		transform: scale(1.2);
+		opacity: 1;
+	}
+	.center-in {
+		transform: scale(1);
+		opacity: 1;
+	}
+	.center-out {
+		transform: scale(0.8);
+		opacity: 1;
+	}
+	.left {
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		left: 0;
+		
+		/* #ifndef APP-PLUS-NVUE */
+		display: flex;
+		/* #endif */
+		flex-direction: row;
+		align-items: stretch;
+		
+		opacity: 0;
+	}
+	.left-init {
+		transform: translateX(-100%);
+		opacity: 1;
+	}
+	.left-in {
+		transform: translateX(0);
+		opacity: 1;
+	}
+	.left-out {
+		transform: translateX(-100%);
+		opacity: 1;
+	}
+	.right {
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		right: 0;
+		
+		/* #ifndef APP-PLUS-NVUE */
+		display: flex;
+		/* #endif */
+		flex-direction: row;
+		align-items: stretch;
+		
+		opacity: 0;
+	}
+	.right-init {
+		transform: translateX(100%);
+		opacity: 1;
+	}
+	.right-in {
+		transform: translateX(0);
+		opacity: 1;
+	}
+	.right-out {
+		transform: translateX(100%);
+		opacity: 1;
+	}
+	.top {
+		position: absolute;
+		top: 0;
+		left: 0;
+		right: 0;
+		opacity: 0;
+	}
+	.top-init {
+		transform: translateY(-100%);
+		opacity: 1;
+	}
+	.top-in {
+		transform: translateY(0);
+		opacity: 1;
+	}
+	.top-out {
+		transform: translateY(-100%);
+		opacity: 1;
+	}
+	.bottom {
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		opacity: 0;
+	}
+	.bottom-init {
+		transform: translateY(100%);
+		opacity: 1;
+	}
+	.bottom-in {
+		transform: translateY(0);
+		opacity: 1;
+	}
+	.bottom-out {
+		transform: translateY(100%);
+		opacity: 1;
+	}
 </style>
