f63dd021e21d247bb3c741294fad6a6fdb26a48c
xghell <605301193@qq.com>
2020-04-20 18:43:15 +0800

    新增order-add页面

---
+++ config/order/order-add.js
@@ -0,0 +1,16 @@
+import style from '@/common/style.js';
+
+const order = {
+	data() {
+		return {
+			moreBtnConfig: {
+				fontFamily: 'xg-iconfont',
+				text: '\ue639',
+				textStyle: `font-size: ${style.UNI_ICON_SIZE_SM}`,
+			},
+			
+		}
+	},
+}
+
+export default order;
